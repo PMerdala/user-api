b739f516f1fca3c17fdb1aaad65bfb69d81298d7
Paweł Merdała <P.Merdala@interia.pl>
2020-12-13 00:59:06 +0100

    First fake implemented user creation

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,7 +1,11 @@
 package users
 
 import (
+	"encoding/json"
+	"github.com/PMerdala/users-api/domain/users"
+	users_service "github.com/PMerdala/users-api/services/users"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -10,5 +14,23 @@ func GetUser(c *gin.Context){
 }
 
 func CreateUser(c *gin.Context){
-	c.String(http.StatusNotImplemented,"Implement me!")
+	var user users.User
+	bytes,err:=ioutil.ReadAll(c.Request.Body)
+	if err!=nil{
+		//TODO: Handle error
+		c.JSON(http.StatusInternalServerError,gin.H{"message":err.Error(),})
+		return
+	}
+	if err:= json.Unmarshal(bytes,&user); err!=nil{
+		//TODO: Handle json error
+		c.JSON(http.StatusInternalServerError,gin.H{"message":err.Error(),})
+		return
+	}
+	result,saveErr:=users_service.CreateUser(user)
+	if saveErr!=nil{
+		//TODO: Handle user creation error
+		c.JSON(http.StatusInternalServerError,gin.H{"message":saveErr.Error(),})
+		return
+	}
+	c.JSON(http.StatusCreated,result)
 }
