791851f5fce3f0e10d155a5738bf984fd06ea2fc
Paweł Merdała <P.Merdala@interia.pl>
2020-12-13 15:47:00 +0100

    validate in user_service

--- services/users/users_service.go
+++ services/users/users_service.go
@@ -3,10 +3,13 @@ package users
 import (
 	"github.com/PMerdala/users-api/domain/users"
 	"github.com/PMerdala/users-api/utils/errors"
-	"net/http"
+	"strings"
 )
 
 func CreateUser(user users.User)(*users.User,*errors.RestErr){
-	var defaultUser users.User
-	return &defaultUser,&errors.RestErr{Message: "Implement me!",Status:http.StatusNotImplemented, Error: "Implement me!"}
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email==""{
+		return nil,errors.NewBadRequestError("invalid email address")
+	}
+	return nil,nil
 }
