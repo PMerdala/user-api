779ad014b96c804b9e9c09ce7936d143be94290c
Paweł Merdała <P.Merdala@interia.pl>
2020-12-13 17:30:16 +0100

    dodanie sprawdzanai istnienia email

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -10,10 +10,10 @@ var (
 	usersDB = make(map[int64]*User)
 )
 
-func (user *User) Get() (*errors.RestErr){
-	result:=usersDB[user.Id]
-	if result==nil {
-		return errors.NewNotFoundError(fmt.Sprintf("User not found by id: %d" ,user.Id))
+func (user *User) Get() *errors.RestErr {
+	result := usersDB[user.Id]
+	if result == nil {
+		return errors.NewNotFoundError(fmt.Sprintf("User not found by id: %d", user.Id))
 	}
 	user.Id = result.Id
 	user.FirstName = result.FirstName
@@ -24,13 +24,27 @@ func (user *User) Get() (*errors.RestErr){
 	return nil
 }
 
-func (user *User) Save() *errors.RestErr{
-	current :=usersDB[user.Id]
-	if current!=nil{
-		if current.Email==user.Email{
-			return errors.NewBadRequestError(fmt.Sprintf("User already exists with email: %s" ,user.Email))
+func (user *User) Save() *errors.RestErr {
+	if user.Id == 0 {
+		user.Id = int64(len(usersDB)) + 1
+	} else {
+		current := usersDB[user.Id]
+		if current != nil {
+			if current.Email == user.Email {
+				return errors.NewBadRequestError(fmt.Sprintf("User already exists with email: %s", user.Email))
+			}
+			return errors.NewBadRequestError(fmt.Sprintf("User already exists with id: %d", user.Id))
 		}
-		return errors.NewBadRequestError(fmt.Sprintf("User already exists with id: %d" ,user.Id))
+	}
+	foundUserId := int64(0)
+	for userId,currentUser:=range usersDB{
+		if currentUser.Email == user.Email{
+			foundUserId=userId
+			break
+		}
+	}
+	if foundUserId != 0{
+		return errors.NewBadRequestError(fmt.Sprintf("User already exists with email: %s", user.Email))
 	}
 	user.DateCreated = date_utils.GetNowString()
 	usersDB[user.Id] = user
