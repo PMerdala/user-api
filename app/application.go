4ecb6bdd9577d6a5cf4c699ddd7ae06ca3827501
Paweł Merdała <P.Merdala@interia.pl>
2020-12-13 19:40:23 +0100

    add connect to mysql

--- app/application.go
+++ app/application.go
@@ -1,12 +1,18 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/PMerdala/users-api/datasources/mysql/users_db"
+	"github.com/gin-gonic/gin"
+)
 
 var (
 	route = gin.Default()
 )
 
 func StartApplication() {
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
 	mapUrls()
 	route.Run(":8080")
 }
---
+++ datasources/mysql/users_db/users_db.go
@@ -0,0 +1,40 @@
+package users_db
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"os"
+)
+
+const (
+	mysql_users_db_username="mysql_users_db_username"
+	mysql_users_db_password="mysql_users_db_password"
+	mysql_users_db_host="mysql_users_db_host"
+	mysql_users_db_schema="mysql_users_db_schema"
+)
+
+var (
+	Client *sql.DB
+
+	username = os.Getenv(mysql_users_db_username)
+	password = os.Getenv(mysql_users_db_password)
+	host= os.Getenv(mysql_users_db_host)
+	schema_db =os.Getenv(mysql_users_db_schema)
+)
+
+func init() {
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
+		username,password,host,schema_db,
+	)
+	var err error
+	Client, err = sql.Open("mysql", dataSourceName)
+	if err != nil {
+		panic(err)
+	}
+	if err = Client.Ping(); err != nil {
+		panic(err)
+	}
+	log.Println("Database successfully configured")
+}
