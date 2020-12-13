e08765b4baba813d11f13ccc7ad6e45375dd2012
Paweł Merdała <P.Merdala@interia.pl>
2020-12-13 15:53:14 +0100

    validate User in package users

--- services/users/users_service.go
+++ services/users/users_service.go
@@ -3,13 +3,11 @@ package users
 import (
 	"github.com/PMerdala/users-api/domain/users"
 	"github.com/PMerdala/users-api/utils/errors"
-	"strings"
 )
 
-func CreateUser(user users.User)(*users.User,*errors.RestErr){
-	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
-	if user.Email==""{
-		return nil,errors.NewBadRequestError("invalid email address")
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	if err := users.Validate(&user); err != nil {
+		return nil, err
 	}
-	return nil,nil
+	return nil, nil
 }
